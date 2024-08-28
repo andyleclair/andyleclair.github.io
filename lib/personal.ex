cb9b7c49a041d896a1ff4834f6640b4f746e1d5f
Andy LeClair <andyleclair@gmail.com>
2024-08-27 20:51:51 -0400

    first commit

---
+++ lib/personal.ex
@@ -0,0 +1,18 @@
+defmodule Personal do
+  @moduledoc """
+  Documentation for `Personal`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Personal.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/personal/application.ex
@@ -0,0 +1,20 @@
+defmodule Personal.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Personal.Worker.start_link(arg)
+      # {Personal.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Personal.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end

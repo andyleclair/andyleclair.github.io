b858ba3749ab2e817681e80a71cce7640f2a0052
Andy LeClair <andyleclair@gmail.com>
2025-10-31 14:39:22 -0400

    make sure all the dirs exist

--- lib/mix/tasks/copy_static.ex
+++ lib/mix/tasks/copy_static.ex
@@ -7,7 +7,10 @@ defmodule Mix.Tasks.CopyStatic do
   def run(_args) do
     Mix.shell().info([:green, "Copying static files"])
 
+    # Ignoring if these exist
     File.mkdir(@output_dir)
+    File.mkdir(@image_dir)
+    File.mkdir(@font_dir)
     File.cp_r!(@font_dir, Path.join([@output_dir, "assets", "fonts"]))
     File.cp_r!(@image_dir, Path.join([@output_dir, "assets", "images"]))
   end

8293b57bece60f8d86258bf7509ddab30992b4c7
Andy LeClair <andyleclair@gmail.com>
2025-11-07 22:07:06 -0500

    gotta pad those strings people

--- lib/mix/tasks/publish_post.ex
+++ lib/mix/tasks/publish_post.ex
@@ -3,28 +3,32 @@ defmodule Mix.Tasks.PublishPost do
 
   @impl Mix.Task
   def run(_args) do
-    {:ok, drafts} = File.ls("./drafts") 
+    {:ok, drafts} = File.ls("./drafts")
 
     indexed_drafts = Enum.with_index(drafts, 1) |> Enum.map(fn {path, i} -> "#{i}. #{path}" end)
 
-    IO.puts """
-      Listing Draft posts:
-      #{Enum.join(indexed_drafts, "\n")}
-      """
+    IO.puts("""
+    Listing Draft posts:
+    #{Enum.join(indexed_drafts, "\n")}
+    """)
 
     index = get_draft()
     draft = Enum.at(drafts, index - 1)
 
-    IO.puts "Publishing #{draft}"
+    IO.puts("Publishing #{draft}")
 
     today = Date.utc_today()
 
-    File.cp!("./drafts/#{draft}", "./posts/#{today.year}/#{today.month}-#{today.day}-#{draft}")
+    month = today.month |> to_string() |> String.pad_leading(2, "0")
+    day = today.day |> to_string() |> String.pad_leading(2, "0")
+
+    File.cp!("./drafts/#{draft}", "./posts/#{today.year}/#{month}-#{day}-#{draft}")
     File.rm!("./drafts/#{draft}")
   end
 
   def get_draft do
-    IO.gets("Which draft post would you like to publish?: ") |> String.trim() |> String.to_integer()
+    IO.gets("Which draft post would you like to publish?: ")
+    |> String.trim()
+    |> String.to_integer()
   end
-  
 end
--- lib/personal/post.ex
+++ lib/personal/post.ex
@@ -17,6 +17,8 @@ defmodule Personal.Post do
     [year, month_day_id] = path |> Path.split() |> Enum.take(-2)
     path = path <> ".html"
     [month, day, id] = String.split(month_day_id, "-", parts: 3)
+    month = String.pad_leading(month, 2, "0")
+    day = String.pad_leading(day, 2, "0")
     date = Date.from_iso8601!("#{year}-#{month}-#{day}")
     struct!(__MODULE__, [id: id, date: date, body: body, path: path] ++ Map.to_list(attrs))
   end

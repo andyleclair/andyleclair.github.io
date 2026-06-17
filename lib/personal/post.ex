0a3c200e70858c4b47b76a8ec3c4405e5fe3b3f0
Jacob Swanner <jacob@jacobswanner.com>
2026-06-17 13:26:33 -0700

    Make post path relative to root
    Links are broken from https://andyleclair.dev/posts/

--- lib/personal/post.ex
+++ lib/personal/post.ex
@@ -23,7 +23,7 @@ defmodule Personal.Post do
   def build(filename, attrs, body) do
     path = Path.rootname(filename)
     [year, month_day_id] = path |> Path.split() |> Enum.take(-2)
-    path = path <> ".html"
+    path = "/" <> path <> ".html"
     [month, day, id] = String.split(month_day_id, "-", parts: 3)
     month = String.pad_leading(month, 2, "0")
     day = String.pad_leading(day, 2, "0")
